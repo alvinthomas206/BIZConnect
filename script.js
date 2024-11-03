d201d8d2b100b0e5682a41c4780ff359002437dd
ALVIN THOMAS <alvinthomas206@gmail.com>
2024-11-03 11:34:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const addToCartButton = document.querySelector('.add-to-cart');
+
+addToCartButton.addEventListener('click', () => {
+  // Simulate adding to cart (replace with actual cart functionality)
+  alert('Product added to cart!');
+
+  // You can also use JavaScript to update a cart icon or display a cart summary.
+});
+
